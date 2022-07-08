4b90de4fb0ce45e34d66063b147cfee9ecbaf955
Nick-I-A <106814650+Nick-I-A@users.noreply.github.com>
2022-07-08 07:51:47 +0200

    Started with multi-language support

--- src/main.js
+++ src/main.js
@@ -3,8 +3,21 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 
+// Custom imports
+import { createI18n } from 'vue-i18n/index'
+import messages from '@/lang'
+
 loadFonts()
+
+// Get the current language by the browser
+const userLang = navigator.language || navigator.userLanguage
+export const i18n = createI18n({
+  locale: userLang,
+  fallbackLocale: 'en',
+  messages
+})
 
 createApp(App)
   .use(vuetify)
+  .use(i18n)
   .mount('#app')
