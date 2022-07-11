490622e1b59c9bb60c1e0e7b9579854e87b34d4e
Nick-I-A <106814650+Nick-I-A@users.noreply.github.com>
2022-07-11 22:29:00 +0200

    Added ServerListItem

--- src/components/ServerList.vue
+++ src/components/ServerList.vue
@@ -20,54 +20,21 @@
       </tr>
     </thead>
     <tbody>
-      <v-dialog v-for="(data, index) in servers" :key="index" v-model="dialogs[index]">
-        <template v-slot:activator="{ props }">
-          <tr v-bind:="props">
-            <td>{{ data.name }}</td>
-            <td>{{ data.address }}:{{ data.port }}</td>
-            <td>{{ data.version }}</td>
-            <td><span class="font-weight-medium">{{ data.players }}</span>/{{ data.maxPlayers > 999 ? '∞' : data.maxPlayers }}</td>
-            <td>{{ data.country ?? '???' }}</td>
-          </tr>
-        </template>
-
-        <v-card>
-          <v-toolbar>{{ data.name }}</v-toolbar>
-          <v-card-text>
-            <div>
-              <span class="font-weight-medium text-uppercase">{{ $t("servers[0].tables[0].modal[0]") }}</span> {{ data.players }}/{{ data.maxPlayers > 999 ? '∞' : data.maxPlayers }}
-            </div>
-            <div v-show="data.language">
-              <span class="font-weight-medium text-uppercase">{{ $t("servers[0].tables[0].modal[1]") }}</span> {{ data.language ?? null }}
-            </div>
-            <div>
-              <span class="font-weight-medium text-uppercase">{{ $t("servers[0].tables[0].modal[2]") }}</span> {{ data.address }}:{{ data.port }}
-            </div>
-            <div v-show="data.gameMode">
-              <span class="font-weight-medium text-uppercase">{{ $t("servers[0].tables[0].modal[3]") }}</span> {{ data.gameMode ?? null }}
-            </div>
-            <div v-show="data.website">
-              <span class="font-weight-medium text-uppercase">{{ $t("servers[0].tables[0].modal[4]") }}</span> {{ data.website ?? null }}
-            </div>
-            <div v-show="data.description">
-              <span class="font-weight-medium text-uppercase">{{ $t("servers[0].tables[0].modal[5]") }}</span> {{ data.description ?? null }}
-            </div>
-          </v-card-text>
-          <v-card-actions>
-            <v-btn color="error" block @click="dialogs[index] = false">{{ $t("servers[0].tables[0].modal[6]") }}</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
+      <serverListItemComponent v-for="(data, index) in servers" :key="index" :server="data" />
     </tbody>
   </v-table>
 </template>
 
 <script>
+import serverListItemComponent from '@/components/ServerListItem.vue'
+
 export default {
   data: () => ({
-    dialogs: null,
     servers: null
   }),
+  components: {
+    serverListItemComponent
+  },
   async mounted() {
     await fetch('https://ragecoop.herokuapp.com/')
           .then(res => res.json())
---
+++ src/components/ServerListItem.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-dialog v-model="dialog">
+    <template v-slot:activator="{ props }">
+      <tr v-bind:="props">
+        <td>{{ server.name }}</td>
+        <td>{{ server.address }}:{{ server.port }}</td>
+        <td>{{ server.version }}</td>
+        <td><span class="font-weight-medium">{{ server.players }}</span>/{{ server.maxPlayers > 999 ? '∞' : server.maxPlayers }}</td>
+        <td>{{ server.country ?? '???' }}</td>
+      </tr>
+    </template>
+
+    <v-card>
+      <v-toolbar>{{ server.name }}</v-toolbar>
+      <v-card-text>
+        <div>
+          <span class="font-weight-medium text-uppercase">{{ $t("servers[0].tables[0].modal[0]") }}</span> {{ server.players }}/{{ server.maxPlayers > 999 ? '∞' : server.maxPlayers }}
+        </div>
+        <div v-show="server.language">
+          <span class="font-weight-medium text-uppercase">{{ $t("servers[0].tables[0].modal[1]") }}</span> {{ server.language ?? null }}
+        </div>
+        <div>
+          <span class="font-weight-medium text-uppercase">{{ $t("servers[0].tables[0].modal[2]") }}</span> {{ server.address }}:{{ server.port }}
+        </div>
+        <div v-show="server.gameMode">
+          <span class="font-weight-medium text-uppercase">{{ $t("servers[0].tables[0].modal[3]") }}</span> {{ server.gameMode ?? null }}
+        </div>
+        <div v-show="server.website">
+          <span class="font-weight-medium text-uppercase">{{ $t("servers[0].tables[0].modal[4]") }}</span> {{ server.website ?? null }}
+        </div>
+        <div v-show="server.description">
+          <span class="font-weight-medium text-uppercase">{{ $t("servers[0].tables[0].modal[5]") }}</span> {{ server.description ?? null }}
+        </div>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn color="error" block @click="dialog = false">{{ $t("servers[0].tables[0].modal[6]") }}</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    dialog: false
+  }),
+  props: ["server", "dialogs"],
+  beforeCreate() {
+    this.dialog = true
+  }
+}
+</script>
