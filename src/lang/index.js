bbe671876b5f4e31742a80c322066dfc7cfcf411
Nick-I-A <106814650+Nick-I-A@users.noreply.github.com>
2022-07-11 21:57:19 +0200

    Added zh-* translations

--- src/lang/index.js
+++ src/lang/index.js
@@ -1,7 +1,14 @@
 import de from './translations/de'
 import en from './translations/en'
 
+import zh_cn from './translations/zh-cn'
+import zh_hk from './translations/zh-hk'
+import zh_tw from './translations/zh-tw'
+
 export default {
-    de,
-    en
+    'de': de,
+    'en': en,
+    'zh-CN': zh_cn,
+    'zh-HK': zh_hk,
+    'zh-TW': zh_tw,
 }
