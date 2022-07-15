1afa6f6766c8ea6afff71c2c772f6808d897b3ed
TheAwesomeKoeniq <106814650+TheAwesomeKoeniq@users.noreply.github.com>
2022-07-15 10:07:22 +0200

    Update ServerList.vue

--- src/components/ServerList.vue
+++ src/components/ServerList.vue
@@ -36,7 +36,7 @@ export default {
     serverListItemComponent
   },
   async mounted() {
-    await fetch('https://test.ragecoop.online/')
+    await fetch('https://masterserver.ragecoop.online/')
           .then(res => res.json())
           .then(data => {
             this.dialogs = new Array(data.length).fill(false)
